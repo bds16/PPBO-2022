54026576cc0e8c71e930c42c665375b88963abe2
bds16 <105010420+bds16@users.noreply.github.com>
2022-10-12 02:45:07 +0700

    Add files via upload

---
+++ Praktikum 4/PPBO_04_Latihan1.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class PPBO_04_Latihan1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ketik bilangan pertama : ");
+        int bil1 = sc.nextInt();
+        System.out.print("Ketik bilangan kedua : ");
+        int bil2 = sc.nextInt();
+        double hasil = Double.valueOf(bil1) / Double.valueOf(bil2);
+        System.out.println(bil1 + " / " + bil2 + " = " + hasil);
+        sc.close();
+    }
+}
+
+/* a. Ketik huruf misalnya f untuk input bilangan pertama kemudian tekan ENTER
+ - Apakah program masih dapat dijalankan setelah user memasukkan f sebagai input untuk bilangan pertama?
+    --> TIDAK
+ - Tulis output yang muncul!
+    --> Exception in thread "main" java.util.InputMismatchException      
+        at java.base/java.util.Scanner.throwFor(Scanner.java:939)
+        at java.base/java.util.Scanner.next(Scanner.java:1594)   
+        at java.base/java.util.Scanner.nextInt(Scanner.java:2258)
+        at java.base/java.util.Scanner.nextInt(Scanner.java:2212)
+        at PPBO_04_Latihan1.main(PPBO_04_Latihan1.java:6)
+ - Jelaskan maksud dari output tersebut! 
+    --> Terjadi exception pada program. Jenis Exceptionnya adalah InputMismatchException yang 
+        terjadi ketika input yang dimasukan tidak sesuai dengan tipe data yang diminta.
+ */
+
+/* b. Pilih salah satu angka sebagai bilangan pertama misal 5, kemudian ketik angka 0 untuk input bilangan kedua. 
+    Jelaskan output yang muncul!
+ - Tulis output yang muncul!
+    --> 16 / 0 = Infinity
+ - Jelaskan maksud dari output tersebut! 
+    --> Output dari hasil pembagian 16 dibagi 0 adalah infinity, karena variabel "hasil" bertipe data double
+        sehingga dimungkinkan pembagian oleh 0.
+    */
---
+++ Praktikum 4/PPBO_04_Latihan2.java
@@ -0,0 +1,36 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class PPBO_04_Latihan2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int bil1, bil2;
+        boolean lanjut = true;
+        while (lanjut) {
+            try {
+                System.out.print("Ketik bilangan pertama : ");
+                bil1 = sc.nextInt();
+                System.out.print("Ketik bilangan kedua : ");
+                bil2 = sc.nextInt();
+                double hasil = Double.valueOf(bil1) / Double.valueOf(bil2);
+
+                System.out.println(bil1 + " / " + bil2 + " = " + hasil);
+                lanjut = false;
+            } catch (InputMismatchException e) {
+                sc.next();
+                System.out.println("Input salah, masukkan angka");
+            }
+        }
+
+        sc.close();
+    }
+}
+
+// Ketik huruf untuk digunakan sebagai input bilangan pertama kemudian klik ENTER. 
+// Jelaskan perbedaannya dengan source code sebelumnya (Latihan1)!
+/* Jawab: 
+    Pada SC ini terdapat try-catch yang berfungsi untuk menghandle exception yang mungkin terjadi.
+    Sedangkan pada Latihan1, tidak terdapat sesuatu yang untuk meng-handle exception sehingga 
+    program akan langsung berhenti.
+ */
